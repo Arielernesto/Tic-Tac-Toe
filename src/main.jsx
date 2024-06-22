562f8dff9813a73782d7b80e1c69a73bd75498d4
Nightmare <fajardoariel435@gmail.com>
2024-06-22 23:16:40 +0200

    Comenzando Tic Tac Toe

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import  App  from './TwitterFollowCard.jsx'  
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <section>
-   <App name="Ariel Fajardo Cardona" username="@ariel435" image="1"/>
-   <App name="Antoni Pol Cardona" username="@antoni435" image="2"/>
-   <App name="Estefani Pol Cardona" username="@estefanipol435" image="3" />
-   <App name="Ramon Pol Duarte" username="@tramp435" image="4" />
-   <App name="Ramon Pol Duarte" username="@tramp435" image="1" />
-   <App name="Ramon Pol Duarte" username="@tramp435" image="4" />
-   <App name="Ramon Pol Duarte" username="@tramp435" image="2" />
-   
-  </section>
-
+  
 )
