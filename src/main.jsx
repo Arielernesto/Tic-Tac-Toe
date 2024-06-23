26c4137469c0c80c72d79b8b29a3eea532964900
Nightmare <fajardoariel435@gmail.com>
2024-06-23 04:13:17 +0200

    Procesando en el tictac toe

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import TicTac from './TicTacToe'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+  <TicTac />
 )
